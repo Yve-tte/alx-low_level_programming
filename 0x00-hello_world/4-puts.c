c76570df503da3a658a8b5e1d1f7831834814245
Yve-tte <vettymatongo@gmail.com>
2023-02-20 13:43:49 -0800

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ *main - The entry point
+ *Return: 0(Success)
  */
-int main
-{	
-puts("\"Programming is like building a multilingual puzzle"); return (0); }
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
