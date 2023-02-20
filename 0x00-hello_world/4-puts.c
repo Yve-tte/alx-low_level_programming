08f37cacdb38e618d580056d9a90e381a984a874
Yve-tte <vettymatongo@gmail.com>
2023-02-20 07:47:16 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts functio
-n
- * Return: 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
-int main(viod)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+int main
+{	
+puts("\"Programming is like building a multilingual puzzle"); return (0); }
