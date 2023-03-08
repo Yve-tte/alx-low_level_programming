14ec0abac2bc986d4d4e9b434c14363940b0cdc6
Yve-tte <vettymatongo@gmail.com>
2023-03-08 12:51:48 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+/** * _puts_recursion - prints a string, followed by a new line * @s: string to print */void _puts_recursion(char *s){	if (*s == '\0')	{		_putchar('\n');		return;	}	_putchar(*s);	_puts_recursion(s + 1);}#include "main.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line
@@ -13,4 +13,3 @@ void _puts_recursion(char *s)
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-}
